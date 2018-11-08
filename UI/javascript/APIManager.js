d9cc44bcd707df96ccdad152bf89eb8d3acc6062
Harry <xuj10@rpi.edu>
2018-11-08 11:45:19 -0500

    Some starter codes

--- UI/javascript/APIManager.js
+++ UI/javascript/APIManager.js
@@ -1,6 +1,29 @@
-let APIManager = function() {};
+let APIManager = function(rankingAddress = '', indexingAddress = '') {
+    this.rankingAddress = rankingAddress;
+    this.indexingAddress = indexingAddress;
+};
+
+APIManager.prototype.setRankingAddress = function(address) {
+    this.rankingAddress = address;
+};
+
+APIManager.prototype.setIndexingAddress = function(address) {
+    this.indexingAddress = address;
+};
 
 APIManager.prototype.getDocument = function(docID) {
+    let headerInit = {
+        Content-Type : 'application/json',
+        Accept-Charset : 'utf-8'
+    };
+    let header = new Headers(headerInit);
+    let requestInit = {
+        method : 'GET',
+        headers : header
+    };
+    let request = new Request(this.rankingAddress, requestInit);
+    // Not sure if this is correct. Working on this
+    // fetch(request).then(function(response) {return respons.json()})
 };
 
 APIManager.prototype.searchRequest = function(queryInfo) {
